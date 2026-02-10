38e8a8f2a4299ebc841c6b906a5484e49c359d99
punyashree <punya8144@gmail.com>
2026-02-10 23:28:53 +0530

    fix: cors header

--- src/app.js
+++ src/app.js
@@ -20,17 +20,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin) {
-        return callback(null, true);
-      }
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("Not allowed by CORS"));
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: allowedOrigins,
   }),
 );
 app.options("*", cors());
